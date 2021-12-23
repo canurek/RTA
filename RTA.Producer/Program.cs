1d9879174be4011d8b6fba0e6ca7a5917a4bd997
can <can@canurek.com>
2021-12-23 22:48:50 +0300

    Producer developments

--- RTA.Producer/Program.cs
+++ RTA.Producer/Program.cs
@@ -1,4 +1,7 @@
+using Confluent.Kafka;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -7,18 +10,35 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!")
+   .WithName("Hello");
 
 app.UseHttpsRedirection();
 
-app.MapPost("/producedata", (string data) =>
+app.MapPost("/producedata", async (string data) =>
 {
-    return $"{data} produced";
+    var host = Environment.GetEnvironmentVariable("ADVERTISED_HOST");
+    var port = Environment.GetEnvironmentVariable("ADVERTISED_PORT");
+
+    var config = new ProducerConfig
+    {
+        BootstrapServers = $"{host}:{port}"
+    };
+
+    using var p = new ProducerBuilder<Null, string>(config).Build();
+
+    try
+    {
+        var dr = await p.ProduceAsync("rtaTestTopic", new Message<Null, string> { Value = data });
+        return $"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'";
+    }
+    catch (ProduceException<Null, string> e)
+    {
+        return $"Delivery failed: {e.Error.Reason}";
+    }
 });
 
 app.Run();
