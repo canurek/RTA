55cd670d77689008ecd79da47111bb71a0e85e38
can <can@canurek.com>
2021-12-25 02:22:44 +0300

    Final Developments

--- RTA.Producer/Program.cs
+++ RTA.Producer/Program.cs
@@ -1,48 +1,18 @@
 using System.Text.Json;
 using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
+using RTA.Producer;
+using RTA.Producer.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSwaggerGen();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointDefinitions(typeof(IAssemblyMarker));
 
 var app = builder.Build();
 
-app.MapSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.MapPost("/producedata", async ([FromBody] Events events) =>
-{
-    var host = Environment.GetEnvironmentVariable("ADVERTISED_HOST");
-    var port = Environment.GetEnvironmentVariable("ADVERTISED_PORT");
-    var topic = Environment.GetEnvironmentVariable("TOPIC");
-
-    var config = new ProducerConfig
-    {
-        BootstrapServers = $"{host}:{port}"
-    };
-
-    using var p = new ProducerBuilder<Null, string>(config).Build();
-
-    try
-    {
-        var json = JsonSerializer.Serialize<Events>(events);
-
-        var dr = await p.ProduceAsync(topic, new Message<Null, string> { Value = json });
-        return $"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'";
-    }
-    catch (ProduceException<Null, string> e)
-    {
-        return $"Delivery failed: {e.Error.Reason}";
-    }
-});
+app.UseEndpointDefinitions();
 
 app.Run();
+
 
 
